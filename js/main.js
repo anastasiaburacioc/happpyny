701e5a4b50604977c3f3dcb6b8a265b91a1240a1
anastasiaburacioc <57499591+anastasiaburacioc@users.noreply.github.com>
2024-12-30 23:51:34 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 
  alert('Te rog să conectezi sunetul! :)');
+
+
 document.body.addEventListener('click', function(){
   let video = document.getElementById("audio");
 video.play();
+ 
+
+
+    let words = document.querySelector('.words');
+    
+words.classList.add('displayNone');
+
+let toy = document.querySelector('.toy');
+toy.classList.add('toy-animation');
+let text = document.querySelector('.text');
+text.classList.remove('displayNone');
+
+text.classList.add('text-animation');
 })
 
