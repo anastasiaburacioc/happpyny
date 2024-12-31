1cd5bd130b67ad58bc997bf2c8fb9975abfd97c7
anastasiaburacioc <57499591+anastasiaburacioc@users.noreply.github.com>
2024-12-31 10:31:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,18 +5,21 @@
 document.body.addEventListener('click', function(){
   let video = document.getElementById("audio");
 video.play();
- 
 
-
-    let words = document.querySelector('.words');
+let words = document.querySelector('.words');
     
 words.classList.add('displayNone');
 
 let toy = document.querySelector('.toy');
 toy.classList.add('toy-animation');
+let fire = document.querySelector('.fire');
+fire.classList.add('fire-animation');
+let fire2 = document.querySelector('.fire-2');
+fire2.classList.add('fire-animation');
+let fire3 = document.querySelector('.fire-3');
+fire3.classList.add('fire-animation');
 let text = document.querySelector('.text');
 text.classList.remove('displayNone');
-
 text.classList.add('text-animation');
 })
 
